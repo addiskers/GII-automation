ffe0a1d3fc9e87f43a2a0a37a0b71cd9b2fb401f
ad <adityaagrawal478@gmail.com>
2024-10-01 12:41:47 +0530

    edge new 3  commit

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def setup_selenium_driver():
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    
+    options.binary_location = "/usr/bin/google-chrome"
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
     
