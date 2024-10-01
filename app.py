8702ecac053a989f4215a191adbbd5b0b6254229
ad <adityaagrawal478@gmail.com>
2024-10-01 13:11:54 +0530

    edge new 4 commit

--- app.py
+++ app.py
@@ -22,14 +22,14 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 
 def setup_selenium_driver():
-    options = Options()
-    options.add_argument("--headless")  
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.binary_location = "/usr/bin/google-chrome"
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=options)
     
     return driver
 
