bdaa80e559aa1106394bd2a3d000b66483e21d87
ad <adityaagrawal478@gmail.com>
2024-11-15 18:53:11 +0530

    driver

--- utils/scraper.py
+++ utils/scraper.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
 from bs4 import BeautifulSoup
 from openai import OpenAI
 from dotenv import load_dotenv
